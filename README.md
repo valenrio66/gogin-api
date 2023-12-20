64c911b7a646214a72fa6bada4d8ad0a17e93e00
valenrio66 <bachtiarramadhan21>
2023-12-20 13:52:23 +0700

    Tambah database dan Readme

---
+++ README.md
@@ -0,0 +1,27 @@
+# Golang Gin MySQL Rest API
+
+## Langkah-langkah Penggunaan
+
+1. **Import Database:**
+
+   - Impor database terlebih dahulu ke localhost masing-masing.
+
+2. **Penamaan Database:**
+
+   - Pastikan nama database yang diimpor sama.
+
+3. **Clone Proyek Ini:**
+
+   - Lakukan clone proyek ini dengan perintah berikut:
+     ```bash
+     git clone git@github.com:valenrio66/gogin-api.git
+     ```
+
+4. **Menjalankan Aplikasi:**
+
+   - Gunakan perintah berikut untuk menjalankan aplikasi:
+     ```bash
+     go run main.go
+     ```
+
+   Aplikasi akan berjalan di `http://localhost:8080`. Silakan buka browser atau aplikasi API testing (seperti Postman) untuk mengakses dan menguji REST API yang telah dibuat.
---
+++ albumdb.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 20, 2023 at 07:49 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `albumdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `album`
+--
+
+CREATE TABLE `album` (
+  `id_album` int(11) NOT NULL,
+  `judul` varchar(200) NOT NULL,
+  `artis` varchar(200) NOT NULL,
+  `harga` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `album`
+--
+
+INSERT INTO `album` (`id_album`, `judul`, `artis`, `harga`) VALUES
+(1, 'Pito', 'Eja', 5000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `album`
+--
+ALTER TABLE `album`
+  ADD PRIMARY KEY (`id_album`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `album`
+--
+ALTER TABLE `album`
+  MODIFY `id_album` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
